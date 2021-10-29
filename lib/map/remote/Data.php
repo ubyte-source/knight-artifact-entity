ab3dc169678fd8a522515c1b4949543090d6c17a
Paolo Fabris <p.fabris@energia-europa.com>
2021-10-29 11:42:31 +0200

    remote data implements response on field and join left

--- lib/map/remote/Data.php
+++ lib/map/remote/Data.php
@@ -10,8 +10,11 @@ use Entity\map\Remote;
 
 class Data
 {
+    const JOINLEFT = 0x1;
+
     protected $remote; // (Remote)
     protected $worker; // (Closure)
+    protected $name;   // (string)
     protected $key;    // (string)
 
     public function __construct(Remote $remote)
@@ -22,6 +25,17 @@ class Data
     public function getRemote() : Remote
     {
         return $this->remote;
+    }
+
+    public function setName(string $name) : self
+    {
+        $this->name = trim($name);
+        return $this;
+    }
+
+    public function getName() :? string
+    {
+        return $this->name;
     }
 
     public function setKey(string $key) : self
@@ -41,7 +55,7 @@ class Data
         return $this;
     }
 
-    public function get(array &$results, array $post = array()) : void
+    public function get(array &$results, int $flags = 0, array $post = array()) : void
     {
         $key = $this->getKey();
         $worker = $this->getWorker();
@@ -54,11 +68,23 @@ class Data
         $remote = $worker->call($this, $post);
         if (false === is_array($remote)) throw new CustomException('entity/remote/not/response/array');
 
+        $locale = $this->getName();
+        $locale = false === is_string($locale) || 0 === strlen($locale)
+            ? null
+            : $locale;
         $remote_column = array_column($remote, $this->getRemote()->getForeign());
         $remote = array_combine($remote_column, $remote);
-        array_walk($results, function (&$value) use ($key, $remote) {
-            if (array_key_exists($value[$key], $remote)) return $value += (array)$remote[$value[$key]];
-            $value = null;
+        array_walk($results, function (&$value) use ($key, $flags, $locale, $remote) {
+            if (false === array_key_exists($value[$key], $remote)) {
+                if ((bool)(static::JOINLEFT & $flags) === false) 
+                    $value = null;
+            } else {
+                if (null === $locale) {
+                    $value += (array)$remote[$value[$key]];
+                } else {
+                    $value[$locale] = (array)$remote[$value[$key]];
+                }
+            }
         });
 
         $results = array_filter($results);
